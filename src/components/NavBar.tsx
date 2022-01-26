5f5a7c1f88a732e3a8aa66e8f8b61055156daba9
rajwinderb <68147202+rajwinderb@users.noreply.github.com>
2022-01-26 19:29:41 +0000

    finish basic structure

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,18 @@
 export default function NavBar(): JSX.Element {
+  // const baseURL = "http://localhost:3000/";
+
+  const baseURL = "https://art-galley.netlify.app/";
+
   return (
     <nav>
-      <p>nav bar</p>
+      <ul className="NavBar">
+        <li>
+          <a href={`${baseURL}`}>Home</a>
+        </li>
+        <li>
+          <a href={`${baseURL}your-art-gallery`}>Your Art Gallery</a>
+        </li>
+      </ul>
     </nav>
   );
 }
--- src/routes/UserGallery.tsx
+++ src/routes/UserGallery.tsx
@@ -1,4 +1,7 @@
+import { useNavigate } from "react-router-dom";
+
 export default function UserGallery(): JSX.Element {
+  useNavigate();
   return (
     <div className="UserGallery">
       <p>User Gallery</p>
